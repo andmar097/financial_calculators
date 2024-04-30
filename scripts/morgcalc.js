089cdf2c145a4b7bf183b70bef2dbfb83a7327be
Andrea Martinez <mandrea781@outlook.com>
2024-04-29 21:22:26 -0500

    fixed js link from index to morgcalc.js, created html for morg calc, created js for morgcals js

---
+++ scripts/morgcalc.js
@@ -0,0 +1,31 @@
+"use strict"
+
+window.onload = init;
+
+function init() {
+
+    let calculateButton = document.querySelector("#calculateButton");
+
+    calculateButton.addEventListener("click", mortgage);
+}
+
+function mortgage() {
+
+    let principal = document.querySelector("#principal").value;
+    let interestRate = document.querySelector("#interest").value;
+    let loanLength = document.querySelector("#loanLength").value;
+
+
+    let loanMonths = loanLength * 12;
+    let interestRateMonthly = ((interestRate / 100) / 12);
+
+    let monthlyPayment = principal * (interestRateMonthly * Math.pow(1 + interestRateMonthly, loanMonths) / (math.pow(1 + interestRateMonthly, loanMonths) - 1))
+    let paymentTotal = monthlyPayment * loanMonths;
+    let interestValue = paymentTotal - principal;
+
+    monthlyPayment = monthlyPayment.toFixed(2);
+    interestValue = interestValue.toFixed(2);
+
+    let msgText = "Your monthly payment is " + monthlyPayment.toFixed(2) + " for " + loanLength + " years.Your total payment is " + paymentTotal.toFixed(2) + " and total interest is " + interestValue.toFixed(2);
+    document.querySelector("#calculationResult").innerHTML = message
+}
